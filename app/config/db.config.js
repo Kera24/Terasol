e632e6f4d52c06173c2c2368d25e44500cca4b84
Kera24 <raazzsah@gmail.com>
2021-06-17 22:17:10 +0545

    First Commit

---
+++ app/config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url: "mongodb+srv://admin:admin@stock.jmrls.mongodb.net/Terasol?retryWrites=true&w=majority"
+};
---
+++ app/models/user.model.js
@@ -0,0 +1,17 @@
+module.exports = mongoose => {
+  var schema = mongoose.Schema(
+    {
+      title: { type: String, required: true, max: 100 },
+    },
+    { timestamps: true }
+  );
+
+  schema.method("toJSON", function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+
+  const User = mongoose.model("user", schema);
+  return User;
+};
